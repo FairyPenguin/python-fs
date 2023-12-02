8bfb809766e88999f1b9ab97f780304c841130ff
FairyPenguin <autorun81@gmail.com>
2023-12-02 12:18:39 +0200

    Data-Types Vars - Input - snake_case naming

---
+++ basics.py
@@ -0,0 +1,23 @@
+# # # comment 
+# # print("Snake is here") #comment too
+
+# # ##data types
+# # print("string") #strings
+# # print(1000) #int
+# # print(10.5) #float 
+
+
+# print(3+6)
+
+# #variables 
+
+# snake = 123
+# # snake = snake +100
+# snake+=100
+# print(snake+2)
+
+# ## snake_case
+# snake_case_var_name = 500
+
+# input 
+
--- snake.py
+++ snake.py
@@ -1,1 +1,12 @@
 print("Snake hmm hmm")
+
+x= 5
+
+y =10
+
+def add (a,b):
+    return a+b
+    
+    
+print(add(x,y))
+
