3bbcaac47d59403e5b6b784dffcd97ad5cd970a9
FairyPenguin <autorun81@gmail.com>
2023-12-06 17:51:13 +0200

    Functions

---
+++ functions_methods.py
@@ -0,0 +1,31 @@
+import math
+
+# function
+# print()
+# input()
+
+# print(input("Enter a value....."))
+
+# methods - > function of data-types
+
+
+
+# print("value".upper())
+# print("value".replace("v","g"))
+
+# new functions
+# print(abs(-1))
+# print(max(10,15))
+# print(len("value"))
+
+
+
+value1 = input("Enter value 1:   ")
+
+value2 = input("Enter value 2:   ")
+
+intger_values_sum = int((value1))**2 + int((value2))**2
+
+print(intger_values_sum)
+ 
+print(round(math.sqrt(intger_values_sum),2))
