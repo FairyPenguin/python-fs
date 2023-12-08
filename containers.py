b998211764890c616ddfe93407c1da33bd51edc0
FairyPenguin <autorun81@gmail.com>
2023-12-08 02:19:35 +0200

    Containers & Slicing

---
+++ containers.py
@@ -0,0 +1,43 @@
+# container types
+
+# list => (1,2,3,4,2,1)
+# tuple => [1,2,3,4,5]
+# set => {1,2,3}
+# dictionary => {"key":"value"}
+
+# convert any container type to a list
+
+
+print(list())
+# convert any container type to a set
+print(set())
+#get the length
+# print(len())
+
+# get values from containers 
+
+# indexing 0 1 2 3 [1]
+# slicing [1,4,1]
+
+# users_list = ["a","b","c","d","e","f",]
+
+# print(users_list[0])
+
+numbers_list = [1,2,3,4,5,6,7,8,9,10]
+
+print(numbers_list[7::-2])
+
+# sliced_numbers = slice(-2)
+
+# sliced = (numbers_list[sliced_numbers])
+
+
+# print(sliced)
+
+# slice_double = (slice(1,8,2))
+
+# reversed = (sliced[slice_double])
+
+# last = reversed[::-1]
+
+# print(last)
