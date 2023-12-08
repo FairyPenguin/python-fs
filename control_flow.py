fda275f56ef06307764cbc1d75d338a72a49a4e6
FairyPenguin <autorun81@gmail.com>
2023-12-08 12:51:48 +0200

    control flow (if statement - while loop - for loop)

---
+++ control_flow.py
@@ -0,0 +1,25 @@
+# control flow - if statement
+
+# if 5 < 4:
+#     print("YES")
+# elif 5 == 5:
+#     print ("They are Euqal")
+# elif 5 > 3 and 5 > 4:
+#     print ("it's Bigger")
+# else: 
+#     print("NO")
+
+# control flow - while loop
+
+# counter = 0
+
+# while counter <= 10:
+#     print(counter)
+#     counter +=1
+
+# control flow - for loop
+
+numbers_list = [1,2,3,4,5,6,7,8,9,10]
+
+for x in numbers_list:
+    print(x)
