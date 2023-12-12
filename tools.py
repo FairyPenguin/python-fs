d6ea2a16d3f8d74c960fed3a2b50df21fb162b20
FairyPenguin <autorun81@gmail.com>
2023-12-12 13:25:52 +0200

    f-strings - one line if statement

---
+++ tools.py
@@ -0,0 +1,14 @@
+# f strings ==> template litreals - string interpolation
+
+user_name = "Johny"
+
+user_age = 10
+
+user_name_and_age = f"{user_name} is {user_age}"
+
+print(user_name_and_age)
+
+# single line if ststement
+user_age= 20
+user_status = "Adult" if user_age >=20 else "teenager"
+print(user_status)
