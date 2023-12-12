97aafef719e1f5edffaf5b62ac659803e85ac2b6
FairyPenguin <autorun81@gmail.com>
2023-12-12 14:20:29 +0200

    lambda functions

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 # f strings ==> template litreals - string interpolation
 
+
 user_name = "Johny"
 
 user_age = 10
@@ -12,3 +13,27 @@ print(user_name_and_age)
 user_age= 20
 user_status = "Adult" if user_age >=20 else "teenager"
 print(user_status)
+
+#list comperhension 
+
+list = [i for i in range(0,11,1)]
+
+# for i in range(0,11,1):
+#     list.append(i)
+
+print(list)
+
+
+# lambda functions
+double_value = lambda num: num * 5
+print(double_value(2))
+
+
+#exercise
+
+battleship_board = [f"{j}{i}"  for j in ("a","b","c","d","e") for i in range(1,6,1)  if f"{j}{i}" != "c3"] 
+
+# if battleship_board == "c3":
+#     battleship_board.remove()
+# else:
+print(battleship_board)
